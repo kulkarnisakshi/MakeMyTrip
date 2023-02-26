52ac0adeeacf088c445321e79d73d625174a545b
sakshi <sakshi@gmail.com>
2023-02-26 17:47:38 +0530

    Application

--- src/main/java/com/example/MakeMyTrip/Cabs.java
+++ src/main/java/com/example/MakeMyTrip/Cabs.java
@@ -1,4 +1,13 @@
 package com.example.MakeMyTrip;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@RestController
+public class Cabs {
+    @GetMapping("/mycabs")
+    public String getData(){
+        return"Please check available cab services";
+    }
+}
